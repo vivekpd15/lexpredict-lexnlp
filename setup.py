532e888909941420ec67c1e078b90b09afa1c59c
VivekP <vivekpd15@gmail.com>
2020-06-23 17:45:32 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
     license='AGPL',
 
     # version ranges for supported Python distributions
-    python_requires='~=3.6',
+    python_requires='~=3.7',
 
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
@@ -64,7 +64,7 @@ setup(
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
 
         # Topics
         'Natural Language :: English',
